d5544efe5f7e345fb0858817870fea91768fca1c
qqqhx0603-crypto <268393933+qqqhx0603-crypto@users.noreply.github.com>
2026-06-30 19:36:48 +0800

    Bump version after rollback

--- scripts/build-debug.ps1
+++ scripts/build-debug.ps1
@@ -62,8 +62,8 @@ Invoke-Checked {
         --java $GeneratedDir `
         --min-sdk-version 26 `
         --target-sdk-version 35 `
-        --version-code 4 `
-        --version-name 0.3.1 `
+        --version-code 6 `
+        --version-name 0.3.3 `
         --auto-add-overlay `
         $CompiledRes
 }
